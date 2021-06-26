2ac3287c0587468af665388afa5b8fd78b209352
Mihaf AJunaidel <mihafsalman11@hotmail.com>
2021-06-26 08:48:58 +0300

    Initial Commit

---
+++ assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assessment
+//
+//  Created by Mihaf on 16/11/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assessmentTests/assessmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  assessmentTests.swift
+//  assessmentTests
+//
+//  Created by Mihaf on 16/11/1442 AH.
+//
+
+import XCTest
+@testable import assessment
+
+class assessmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
